2f794d4fdfff9efc6defb1640fc659e3a76054c0
alexio42 <116265425+alexio42@users.noreply.github.com>
2023-10-15 19:19:48 +0100

    Add multiple operator presses, display 0 correctly
    Add ability to press multiple operators before equals.
    Fixes 0 displaying as exponential

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let num2;
 let currentOperator;
 let result;
 let startNewNumber;
+let clickedOnce;
 
 function add(num1, num2) {
     return ((num1 * 1000000000) + (num2 *1000000000)) / 1000000000;
@@ -68,6 +69,7 @@ allClear.addEventListener("click", function() {
     num1 = "";
     num2 = "";
     currentOperator = "";
+    clickedOnce = "no"
     clicked(this.id);
 });
 
@@ -100,30 +102,43 @@ plusMinus.addEventListener("click", function() {
 // add event listener to operators
 for (const operation of operators) {
     operation.addEventListener("click", function() {
-    
-    currentOperator = this.id;  
 
-    if (display.textContent == "") {
-        num1 = 0
+    
+    if (clickedOnce == 'yes') {
+        num2 = Number(display.textContent);
+        operateFunction = (window[currentOperator]);
+        result = calculate(num1, num2, operateFunction);
+        displayResult(result);
+        currentOperator = '';
+        num1 = Number(display.textContent);
+    } else if (display.textContent == "") {
+        num1 = 0;
+        clickedOnce = 'yes';
     } else {
         num1 = Number(display.textContent);
+        clickedOnce = 'yes';
     };
-    
-    clicked(currentOperator);
+
+    currentOperator = this.id; 
     startNewNumber = 'yes';
+   
+    clicked(this.id)
 
   });
 };
 
 
 equals.addEventListener("click", function() {
-    num2 = Number(display.textContent);
 
+    clicked(this.id);
+    num2 = Number(display.textContent);
     operateFunction = (window[currentOperator]);
     result = calculate(num1, num2, operateFunction);
     displayResult(result);
-    clicked(this.id);
+    clickedOnce = 'no';
+
 })
+
 
 
 
@@ -140,7 +155,7 @@ function clicked(currentKey) {
 
 
   function displayResult(num) {
-    if (num < 0.0000001) {
+    if (num < 0.0000001 && num > 0) {
          display.textContent = num.toExponential(2);
     } else if (num > 999999999) {
          display.textContent = num.toExponential(2);
@@ -221,3 +236,43 @@ function inputNumKeyboard(key) {
 flip.addEventListener("click", function () {
     display.classList.toggle("flipped");
 });
+
+
+// for (const operation of operators) {
+//   operation.addEventListener("click", function() {
+
+//   if (clickedOnce = 'yes'){
+//     num2 = Number(display.textContent);
+//     operateFunction = (window[currentOperator]);
+//     result = calculate(num1, num2, operateFunction);
+//     displayResult(result);
+//     clicked(this.id);
+//     currentOperator = this.id;
+    
+//   } else {
+//   currentOperator = this.id; 
+//   clickedOnce = 'yes';  
+//       if (display.textContent == "") {
+//       num1 = 0
+//   } else {
+//       num1 = Number(display.textContent);
+//   };
+//   };
+
+  
+//   clicked(this.id);
+//   startNewNumber = 'yes';
+
+//   });
+// };
+
+
+// equals.addEventListener("click", function() {
+//     num2 = Number(display.textContent);
+
+//     operateFunction = (window[currentOperator]);
+//     result = calculate(num1, num2, operateFunction);
+//     displayResult(result);
+//     clicked(this.id);
+//     currentOperator = '';
+// });
